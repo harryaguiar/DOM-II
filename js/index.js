045f06212be4f24930dc3232da26a599ff2497c7
Harrison Aguiar <harryaguiar@gmail.com>
2020-08-10 22:23:07 -0400

    added another 2 events

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@
 const buttonTags = document.querySelectorAll('.btn');
 
 buttonTags.forEach((e) => {
+    // event 1 - MOUSEOVER
     e.addEventListener('mouseover', () => {
     e.style.transform = "scale(1.5)";
     e.style.transition = "transform 1s";
 });
 
+// event 2 - MOUSEOUT
 e.addEventListener('mouseout', () => {
     e.style.transform = "scale(1)";
 });
@@ -16,6 +18,7 @@ e.addEventListener('mouseout', () => {
 
 const destinationClass = document.querySelectorAll('.destination');
 
+// event 3 - RESIZE
 window.addEventListener("resize", () => {
     destinationClass.forEach((e) => {
         e.style.backgroundColor = "#FFFDDD";
@@ -25,13 +28,58 @@ window.addEventListener("resize", () => {
 
 const h4Tags = document.querySelectorAll("h4");
 h4Tags.forEach((e) =>{
+    // event 4 - DBLCLICK
     e.addEventListener('dblclick', () => {
     e.style.backgroundColor = "blue";
     e.style.color = "white";
 });
 });
 
+
 const containerTag = document.querySelector('body');
+// event 5 - SCROLL
 window.addEventListener('scroll', () => {
     containerTag.style.backgroundColor = "orange";
 });
+
+// event 6 - AUXCLICK
+let h1Tag = document.querySelector('h1');
+
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function randomColor() {
+    return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+}
+
+h1Tag.onclick = function() {
+    h1Tag.style.backgroundColor = randomColor();
+};
+
+h1Tag.onauxclick = function(e) {
+  e.preventDefault();
+  h1Tag.style.color = randomColor();
+}
+
+h1Tag.oncontextmenu = function(e) {
+  e.preventDefault();
+}
+
+
+// event 7 - ONLOAD
+let onLoad = document.querySelectorAll(".nav-link");
+for(let i=0; i<onLoad.length; i++)
+{onLoad[i].addEventListener("onload", function(){
+})}
+console.log('Navigation bar has loaded!', onLoad);
+
+// #5 MOUSEENTER
+let footerList = document.querySelector(".footer");
+
+footerList.addEventListener("mouseenter", function( event ) {   
+  event.target.style.color = "white";
+  setTimeout(function() {
+    event.target.style.color = "white";
+  }, 300);
+}, false);
